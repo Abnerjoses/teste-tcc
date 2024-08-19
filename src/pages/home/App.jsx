1b3a12537b7e10b3597d558d464c4122f694a71d
abner jose <abnerjose512@gmail.com>
2024-08-19 00:17:44 -0300

    subiu

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,37 @@
+import './App.scss';
+
+function App() {
+  return (
+    
+    <div className="valhalla">
+      <header>
+
+        <div className='img-val'>
+
+        <img src="/assets/valhalla-logo.png" alt="" />
+
+        </div>
+        
+        <div className='as'>
+
+        <a href="">Criação</a>
+        <a href="">Recursos</a>
+        <a href="">Preços</a>
+        <div>
+
+        <a href="" className='traco'>|</a>
+        </div>
+
+        </div>
+      
+        <div className='mundo'>
+          <img src="/assets/image.png" alt="" />
+          <button>Entrar</button>
+        </div>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
