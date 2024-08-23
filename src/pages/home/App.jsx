e35181f9122d358eb2842c76466a8d48fd2ef24b
Joao <joaoeric35@gmail.com>
2024-08-23 15:11:33 -0300

    2

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -37,18 +37,12 @@ function App() {
       <h4>EXEMPLOS DE SITES FEITOS COM VALHALLA</h4>
 
       <div className='centro'>
-      
-      <div className='left'>
-      
-      <h1>A inspiração de web <br />design que você estava <br />procurando</h1>
+       
+      <h1>A inspiração de web design que você estava procurando</h1>
 
-      </div>
-
-      <div className='right'>
       <p>
           Explore estes exemplos de sites criados <br /> por usuários Valhalla reais e obtenha <br />inspiração de web design e práticas <br />recomendadas para criar seu site da <br />maneira que você quiser</p>
-
-      </div>
+    
     </div>
 
       </div>
