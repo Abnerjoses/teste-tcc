359e123963ebc8a792a4a1b7a95ae3912e97636b
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-08-22 23:43:00 -0300

    subindo

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -30,6 +30,27 @@ function App() {
         </div>
     
       </header>
+      <div className='mãe1'>
+
+        <div className='textos'>
+          <h1 className='titulo1'> Exemplos de sites feitos com Valhalla</h1>
+        <h2 className='titulo2'>A inspiração de web design que você estava procurando.</h2></div>
+
+        
+        
+        </div>
+
+        <div className='mãe2'>
+        <p className='texto'>
+        Explore estes exemplos de sites criados por usuários Valhalla reais e obtenha inspiração de web design e práticas recomendadas para criar seu site da maneira que você quiser</p>
+
+        <hr className='quebra-de-linha'/>
+
+        <button  className='botão' href='#' >Crie seu site</button>
+
+
+
+      </div>
     </div>
   );
 }
