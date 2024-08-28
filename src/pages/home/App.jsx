b6d2e686f84ec35536fbf4304915a54c24a3c7c9
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-08-27 22:39:08 -0300

    subiu

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -2,94 +2,179 @@ import './App.scss';
 
 function App() {
   return (
-    
+
     <div className="valhalla">
       <header>
 
         <div className='img-val'>
 
-        <img src="/assets/valhalla-logo.png" alt="" />
+          <img src="/assets/valhalla-logo.png" alt="" />
 
         </div>
-        
+
         <div className='as'>
 
-        <a href="">Criação</a>
-        <a href="">Recursos</a>
-        <a href="">Preços</a>
-        <div>
+          <a href="">Criação</a>
+          <a href="">Recursos</a>
+          <a href="">Preços</a>
+          <div>
 
-        <a href="" className='traco'>|</a>
+            <a href="" className='traco'>|</a>
+          </div>
+
         </div>
 
-        </div>
-      
         <div className='mundo'>
           <img src="/assets/image.png" alt="" />
           <button>Entrar</button>
         </div>
-    
+
       </header>
 
       <div className='corpo'>
-     
 
-      <h4>EXEMPLOS DE SITES FEITOS COM VALHALLA</h4>
 
-      <div className='centro'>
-       
-      <h1>A inspiração de web design que você estava procurando</h1>
+        <h4>EXEMPLOS DE SITES FEITOS COM VALHALLA</h4>
 
-      <p>
-          Explore estes exemplos de sites criados <br /> por usuários Valhalla reais e obtenha <br />inspiração de web design e práticas <br />recomendadas para criar seu site da <br />maneira que você quiser</p>
-    
-    </div>
+        <div className='centro'>
 
-    <div className='segunda'>
+          <h1>A inspiração de web design que você estava procurando</h1>
 
-      <hr className='linha'/>
+          <p>
+            Explore estes exemplos de sites criados <br /> por usuários Valhalla reais e obtenha <br />inspiração de web design e práticas <br />recomendadas para criar seu site da <br />maneira que você quiser</p>
 
-      <button className='botao'>Crie seu site</button>
+        </div>
 
-    </div>
-    <div className='terceira'> 
+        <div className='segunda'>
 
-      <img  className='print'src="/assets/images/image-removebg-preview.png"/>
-      
-      
-      
-      <h1 className='titulo'>Vikings</h1>
-      <h2 className='subtitulo'>Explorar site</h2>
-   
+          <hr className='linha' />
+
+          <button className='botao'>Crie seu site</button>
+
+        </div>
+        <div className='terceira'>
+
+          <img className='print' src="/assets/images/image-removebg-preview.png" />
 
 
 
-    </div>
-
-    <div className='quarta'>
-      <h1>Envie seu site para ter a chance<br/>
-      de aparecer em nossa página</h1>
-
-      <button className='button'>Enviar</button>
-    </div>
-
-    <div className='quinta'>    
-    <h1>Okla</h1>
-      
-      <img  className='print1'src="/assets/images/image12.png"/>
-     
+          <h1 className='titulo'>Vikings</h1>
+          <h2 className='subtitulo'>Explorar site</h2>
 
 
-    </div>
 
 
-          
+        </div>
 
-  
+        <div className='quarta'>
+          <h1>Envie seu site para ter a chance<br />
+            de aparecer em nossa página</h1>
+
+          <button className='button'>Enviar</button>
+        </div>
+
+
+
 
       </div>
 
+
+      <div className='sexta'>
+
+        <h1 className='titulo'>FAQ</h1>
+
+        <hr className='linha' />
+      </div>
+
+
+      <div className='salve2'>
+
+        <h2 className='titulo'>Como posso dar um visual mais profissional ao meu site?</h2>
+
+        <p className='texto'>Ao criar um site pela primeira vez, é importante explorar exemplos de sites e escolher um template que atenda às suas necessidades específicas. Recomendamos verificar os sites dos concorrentes para ver como eles incorporam o web design e as funcionalidades para construir sites atraentes. Não se esqueça de que é fundamental manter o design simples. Você não deve sobrecarregar os visitantes com muitas distrações, mas vale a pena acrescentar vídeos e imagens relevantes e de alta qualidade para tornar a experiência do usuário mais interessante. Se precisar de mais inspirações de web design, você pode pesquisar na internet. Há milhares de sites que podem te ajudar.
+        </p>
+
+
+
+      </div>
+      <div className='sexta'>
+
+      
+
+        <hr className='linha' />
+      </div>
+
+
+      <div className='salve2'>
+
+        <h2 className='titulo'>Como posso dar um visual mais profissional ao meu site?</h2>
+
+        <p className='texto'>Ao criar um site pela primeira vez, é importante explorar exemplos de sites e escolher um template que atenda às suas necessidades específicas. Recomendamos verificar os sites dos concorrentes para ver como eles incorporam o web design e as funcionalidades para construir sites atraentes. Não se esqueça de que é fundamental manter o design simples. Você não deve sobrecarregar os visitantes com muitas distrações, mas vale a pena acrescentar vídeos e imagens relevantes e de alta qualidade para tornar a experiência do usuário mais interessante. Se precisar de mais inspirações de web design, você pode pesquisar na internet. Há milhares de sites que podem te ajudar.
+        </p>
+
+
+
+      </div>
+
+      <div className='sexta'>
+
+     
+
+        <hr className='linha' />
+      </div>
+
+
+      <div className='salve2'>
+
+        <h2 className='titulo'>Como posso criar o design do meu site sem prejudicar seu desempenho?</h2>
+
+        <p className='texto'>A Valhalla possui uma infraestrutura robusta e focada na performance para que seu site esteja equipado com todos os recursos necessários para gerenciar ativamente o seu desempenho. Ao criar seu site e procurar por tendências de design, lembre-se que adicionar muitos elementos de design pode aumentar o seu tempo de carregamento. Por esse motivo, os sites com os melhores designs não usam muitas imagens, vídeos e fontes diferentes. Também é uma boa ideia observar o desempenho de seu site no painel Velocidade do site durante a criação para poder otimizar e testar seus tempos de carregamento. Após publicar seu site, verifique os Core Web Vitals regularmente para ver o desempenho do seu site e garantir que você está proporcionando a melhor experiência possível aos visitantes.
+        </p>
+
+
+
+      </div>
+
+
+      <div className='sexta'>
+
+
+        <hr className='linha' />
+      </div>
+
+
+      <div className='salve2'>
+
+        <h2 className='titulo'>Quem pode se beneficiar dos sites inspiradores?</h2>
+
+        <p className='texto'>Qualquer pessoa que esteja criando uma presença online pode recorrer aos sites inspiradores. Não importa se você está criando por conta própria ou seja um profissional em web design, criar um site atraente e bem elaborado pode impulsionar seu sucesso online. Além disso, ao procurar por sites inspiradores, você pode encontrar coisas para acrescentar ao seu site que você ainda não havia pensado, mas que podem melhorar muito a experiência de seus usuários.
+        </p>
+
+
+
+      </div>
+
+      <div className='ultima '>
+
+      <img  className='logo1'src="/assets/valhalla-logo.png" alt="" />
+
+      <p className='textinho'>O criador de sites Valhalla oferece uma solução <br/>completa para negócios. Com infraestrutura de nível<br/> empresarial e ferramentas avançadas de marketing e<br/> SEO, você pode criar um site gratuito e impulsionar sua <br/> presença online.</p>
+
+      </div>
+
+      <div className='loguinhos'>
+      <img  className='logo1'src="/assets/ok.png" alt="" />
+        
+
+      </div>
+
+
+
+
     </div>
+
+
+
   );
 }
 
